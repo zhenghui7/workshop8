fc5ceda90d6051da50dc794151d07350d157574b
ZhengHui <lim_z_hui@hotmail.com>
2023-02-10 11:04:21 +0800

    My first commit

---
+++ src/main/java/sg/edu/nus/iss/app/App.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
